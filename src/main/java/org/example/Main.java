b8a6c0a29d94f1b0eff2530af64739be7f69f830
BadHard <BadHard101@yandex.ru>
2024-01-23 08:00:22 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public class Main {
+    public int[] productExceptSelf(int[] nums) {
+        int[] answer = new int[nums.length];
+
+        int temp = 1;
+        int zerosNum = 0;
+        for (int num : nums) {
+            if (num == 0) {
+                zerosNum++;
+                if (zerosNum == 2) return answer;
+            } else temp *= num;
+        }
+
+        if (zerosNum == 0) {
+            for (int i = 0; i < nums.length; i++) {
+                answer[i] = temp / nums[i];
+            }
+        } else {
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == 0) {
+                    answer[i] = temp;
+                    return answer;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
